900063e7d6bace0f2deaf7d92159adae2aa92b18
MahendraSondagar <mahendrasondagar08@gmail.com>
2021-06-20 23:25:12 +0530

    Stack code series

---
+++ Stack/stack_01.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#define STACK_SIZE  20
+/* Stack configuration */
+typedef struct 
+{
+  int top;
+  int count;
+  int buff[STACK_SIZE];
+}Stack_config_t;
+
+/* init stack by putting top =-1*/
+void Stack_init(Stack_config_t *StackConfig)
+{
+  StackConfig->top =-1;  
+}
+
+/* Stack emptyness check */
+uint8_t isStackEmpty(Stack_config_t *StackConfig)
+{
+  if(StackConfig->top == -1)
+  {
+     return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+/* Stack full check */
+uint8_t isStackFull(Stack_config_t *StackConfig)
+{
+   if(StackConfig->top == STACK_SIZE-1)
+   {
+      return 1;
+   }
+   else
+   {
+     return 0;
+   }
+}
+/* pushing in to the stack from bottom to top */
+void Stack_push(Stack_config_t *StackConfig,int byte)
+{
+    if(!isStackFull(StackConfig))
+    {
+      StackConfig->buff[++StackConfig->top] = byte;
+      StackConfig->count++;
+    }
+    else
+    {
+        printf("Stack is Full\r\n");
+    }
+  
+}
+/* poping the member from the stack */
+void Stack_pop(Stack_config_t *StackConfig)
+{
+    if(isStackEmpty(StackConfig))
+    {
+     printf("Stack is Empty\r\n");
+    }
+    else
+    {
+       printf("poped item is : %d\r\n",   StackConfig->buff[StackConfig->top]);
+       StackConfig->buff[StackConfig->top--];
+       StackConfig->count--;
+    }
+}
+
+/* printing the stack */
+void Stack_print(Stack_config_t *StackConfig)
+{
+    for(int i=0; i<StackConfig->count; i++)
+    {
+       printf("%4d ---> %d\r\n", StackConfig->buff[i],i);
+    }
+}
+
+int main(void)
+{
+ printf("Stack test...\r\n");
+ Stack_config_t StackCfg;
+ /*init stack by top =-1 */
+
+ while(1)
+ {
+     uint8_t cmd;
+     int byte=0;
+     printf("-------------------------\r\n");
+     printf("1> To create the Stack   \r\n");
+     printf("2> To push the Stack     \r\n");
+     printf("3> To pop the Stack      \r\n");
+     printf("4> To print the Stack    \r\n");
+     printf("4> Empty check for Stack \r\n");
+     printf("-------------------------\r\n");
+     printf("Enter the command: ");
+     scanf("%d", &cmd);
+     switch (cmd)
+     {
+     case 1:
+         Stack_init(&StackCfg);
+         break;
+
+    case 2:
+         printf("Enter the byte: ");
+         scanf("%d", &byte);
+         Stack_push(&StackCfg, byte);
+         break;
+
+    case 3:
+         Stack_pop(&StackCfg);
+         break;
+    
+    case 4:
+         Stack_print(&StackCfg);
+         break;
+
+    case 5:
+         isStackEmpty(&StackCfg);
+         break;
+
+    case 6:
+         isStackFull(&StackCfg);
+         break;
+
+     default:
+         printf("Invald command\r\n");
+         break;
+     }
+ }
+ return 0;
+}
---
+++ Stack/stack_02.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define STACK_SIZE 10
+
+/* Stack data structure example with using the dynamic memory allocation*/
+
+/*Creating the stack structure*/
+typedef struct 
+{
+ int count;
+ int top;
+ int *buff;
+}StackInfo_t;
+
+/* Stack init */
+StackInfo_t *Stack_init(StackInfo_t *stack)
+{
+   stack = (StackInfo_t *)malloc(sizeof(StackInfo_t));
+   stack->count=0;
+   stack->top =-1;
+   /* Dynamicaclly assign the memory to array */
+   stack->buff = (int *)malloc(sizeof(int)* STACK_SIZE);
+   return stack;
+}
+
+/*Stack Whether the stac is empty or not  */
+uint8_t isEmpty(StackInfo_t *stack)
+{
+    if(stack->top ==-1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+/* Checking whether the stack is full or not*/
+uint8_t isFull(StackInfo_t *stack)
+{
+  if(stack->top == STACK_SIZE)
+  {
+      return 1;
+  }
+  else
+  {
+      return 0;
+  }
+}
+
+/* pushing the bytes in to the stack from bottom to top*/
+void Stack_push(StackInfo_t *stack, int byte)
+{
+  if(!isFull(stack))
+  {
+      stack->buff[++stack->top] = byte;
+      stack->count++;
+  }
+}
+/* popping the byte from the stack */
+void Stack_pop(StackInfo_t *stack)
+{ 
+    if(!isEmpty(stack))
+    {
+        printf("Popped data: %d\r\n", stack->buff[stack->top]);
+        stack->buff[stack->top--];
+        stack->count--;
+    }
+    else
+    {
+      printf("Stack is Empty!\r\n");
+    }
+  
+}
+
+/*Displaying the stack */
+void Stack_Display(StackInfo_t *stack)
+{
+    for(int i=0; i<stack->count; i++)
+    {
+       printf("%4d\t @ %4d\r\n", stack->buff[i], i); 
+    } 
+}
+
+
+int main(void)
+{
+ StackInfo_t *stack;
+ stack = Stack_init(stack);
+ Stack_push(stack, 10);
+ Stack_push(stack, 20);
+ Stack_push(stack, 30);
+ Stack_push(stack, 40);
+
+ Stack_Display(stack);
+ Stack_pop(stack);
+ Stack_Display(stack);
+
+ return 0;
+
+}
